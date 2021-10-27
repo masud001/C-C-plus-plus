f5dcd38f3dc3465e81b60f2b41308c6820004600
masud001 <madhnagar@gmail.com>
2021-10-28 03:15:32 +0600

    input output in c++

--- inputOutput.cpp
+++ inputOutput.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
     int num1, num2;
-    cout << "Enter the num1 : ";
+    cout << "Enter the num1 here : ";
     cin >> num1;
-    cout << "Enter the num2 : ";
+    cout << "Enter the num2 here : ";
     cin >> num2;
     cout << "the total value is  : " << num1 + num2;
     return 0;

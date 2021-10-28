745f128eb18956d05d4a73ce4640a7e9dc5ce75b
masud001 <madhnagar@gmail.com>
2021-10-29 04:00:45 +0600

    manipulator in c++

---
+++ manipulator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int a = 1, b = 22, c = 333, d = 4444;
+    cout << setw(2) << a << endl;
+    cout << setw(2) << b << endl;
+    cout << setw(2) << c << endl;
+    cout << setw(2) << d << endl;
+    return 0;
+}
